0999986c3535b683aa7b179fe7c97e58e537323d
Marko Novakovic <marfy87@gmail.com>
2024-03-21 10:08:55 -0400

    finish middleware, return jwt

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -6,7 +6,7 @@ import CompatibilityChart from "../models/compatibilityChart.js";
 
 import { ApolloError } from "apollo-server-errors";
 import bcrypt from "bcryptjs";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 export const resolvers = {
@@ -98,7 +98,7 @@ export const resolvers = {
       });
 
       // Create our JWT (attach to out User model)
-      const token = Jwt.sign(
+      const token = jwt.sign(
         { user_id: newUser._id, email },
         process.env.JWT_STRING,
         {
@@ -123,7 +123,7 @@ export const resolvers = {
       // Check if entered password equals the encrypted password
       if (user && (await bcrypt.compare(password, user.password))) {
         // Create a NEW token
-        const token = Jwt.sign(
+        const token = jwt.sign(
           { user_id: newUser._id, email },
           process.env.JWT_STRING,
           {
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,3 +1,22 @@
 import { AuthenticationError } from "apollo-server-errors";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
-import { Jwt } from "jsonwebtoken";
+module.exports = (context) => {
+  const authHeader = context.req.headers.authorization;
+
+  if (authHeader) {
+    // Bearer ....
+    const token = authHeader.split("Bearer")[1];
+    if (token) {
+      try {
+        const user = jwt.verify(token, process.env.JWT_STRING);
+        return user;
+      } catch (err) {
+        throw new AuthenticationError("Invalid/Expired token");
+      }
+    }
+    throw new Error("Authentication token must be 'Bearer [token]");
+  }
+  throw new Error("Authorization header must be provided");
+};
